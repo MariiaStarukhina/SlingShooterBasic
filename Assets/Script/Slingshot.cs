35b2f26b25215d85ea6115166e2a0d4f53265de2
Mariia <Mariia.Starukhina@stud.h-da.de>
2015-05-11 16:00:08 +0200

    Created a new slingshot game.

---
+++ Assets/Script/Slingshot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public GameObject launchPoint;
+	void Awake() {
+		Transform launchPointTransform = transform.Find ("launchPoint");
+		launchPoint = launchPointTransform.gameObject;
+		launchPoint.SetActive(false);
+	}
+	void OnMouseEnter() {
+		launchPoint.SetActive(true);
+		//print ("Yay, the mouse has entered");
+	}
+
+	void OnMouseExit() {
+		launchPoint.SetActive(false);
+		//print ("Oh no, the mouse has exited");
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
